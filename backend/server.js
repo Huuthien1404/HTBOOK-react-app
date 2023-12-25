5f6d3f46f861af659a9bf0ab45cdb432a5c2c845
Huuthien1404 <thie1232135@gmail.com>
2023-12-25 17:53:01 +0700

    dfjksdf

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(
     resave: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      sameSite: "lax",
+      sameSite: "none",
       secure: false,
       httpOnly: true,
     },
